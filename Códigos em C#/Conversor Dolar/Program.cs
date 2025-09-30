f0135655996890c071709797f678149688760fab
Henrique da Cruz Lima <cruzhenrique263@gmail.com>
2025-09-29 22:18:12 -0300

    Add files via upload

---
+++ Códigos em C#/Conversor Dolar/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conversor_Dolar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1° Passo - Declarar as variaveis
+            float contaçao_dolar, quantidade_dolar, total_reais;
+            //2° Passo - Reber os valores(Entrada)
+            Console.WriteLine("Bem vindo ao conversor do Dolares para Reais");
+
+            Console.Write("Digite a contação atual do dolar: ");
+            contaçao_dolar = float.Parse(Console.ReadLine());
+
+            Console.Write("Digite a quantia a ser Convertida: ");
+            quantidade_dolar = float.Parse(Console.ReadLine());
+            //3° Passo - Processamento
+            total_reais = quantidade_dolar * contaçao_dolar;
+            //4° Passo - Saída
+            Console.WriteLine("O valor convertido é:" + total_reais);
+
+            Console.WriteLine("Aperte a tecla ENTER para sair.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Códigos em C#/Converter Temperatura/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Converter_Temperatura
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1°Passo - Declarar as variaveis
+            float temperaturac, temperaturaf;
+
+            //2°Passo - Receber os valores(ENTRADAS)
+            Console.WriteLine("Conversor de Temperatura");
+
+            Console.Write("Entre com a Temperatura em C°: ");
+            temperaturac = float.Parse(Console.ReadLine());
+
+
+            //3°Passo - Processamento
+            temperaturaf = (9 * temperaturac + 160) / 5;
+            //4°Pass - Saída
+            Console.Write("A temperatura em Fahrenheit é: " + temperaturaf);
+
+
+
+
+
+
+
+
+            Console.WriteLine("\n Aperte a tecla ENTER para fechar o programa");
+            Console.ReadKey();
+        }
+    }
+}
