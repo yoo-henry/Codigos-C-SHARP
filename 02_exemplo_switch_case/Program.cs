abc1f9199686c58e80e6ec73f641d0ec47e41585
Henrique da Cruz Lima <cruzhenrique263@gmail.com>
2025-10-20 21:22:30 -0300

    Add files via upload

---
+++ 02_exemplo_switch_case/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_exemplo_switch_case
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1° - Declarar a variavel
+            string cor;
+            //2° - Entrada
+            Console.Write("Digite uma cor em inglês:");
+            cor = Console.ReadLine().ToUpper();
+
+            switch (cor)
+            {
+                case "GREEN":
+                    Console.Write("A cor digitada em PT-BR é verde");
+                    break;
+                case "BLUE":
+                    Console.Write("A cor digitada em PT-BR é azul");
+                    break;
+                case "RED":
+                    Console.Write("A cor digitada em PT-BR é vermelho");
+                    break;
+                case "YELLOW":
+                    Console.Write("A cor digitada em PT-BR é amarelo");
+                    break;
+                default:
+                    Console.Write("Cor não identificada");
+                    break;
+
+
+            }
+
+
+        }
+    }
+}
